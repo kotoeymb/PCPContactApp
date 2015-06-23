8b0635da5b469c6564fa2adeee9230ca2877ddda
Shwe Sin May Oo <shwesinmayoo@pcp.co.jp>
2015-06-23 13:55:24 +0630

    menu update

--- app/controllers/edit.js
+++
@@ -1,5 +0,0 @@
-
-var args = arguments[0] || {};
-// console.log("title:" + args.title + ", author: " + args.author);
-$.titleLabel.text = args.item  ||'Default name';
-$.phonelabel.text = args.date_completed || 'Default phone';
