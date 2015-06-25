f43f065f9f336057425a021b38cf31fc6a3c9a89
Gabriel Westin <pyinyeinkyaw@pcp.co.jp>
2015-06-25 10:17:45 +0630

    favourite fixed

--- app/controllers/favourite.js
+++ app/controllers/favourite.js
@@ -1,14 +1,29 @@
 var todos = Alloy.Collections.todo;
-todos.fetch();
 
-$.backButton.addEventListener('click', 
-    function(e) {
-        
-        var index = Alloy.createController('index').getView();
-        
-        index.open({
-            transition : Ti.UI.iPhone.AnimationStyle.FLIP_FROM_RIGHT
-        });
-        
-    }
-);
+var INDEXES = {
+	'NotFav': 0,
+	'YesFav': 1,
+};
+var whereIndex = INDEXES['YesFav'];
+
+function whereFunction(collection) {
+	return !whereIndex ?
+		collection.models :
+		collection.where({ done: whereIndex });
+}
+
+function toggleFav(e) {
+	var s = e.source;
+	Alloy.Collections.todo.updateRecord({
+		query : {
+			columns : ["done"],
+			values : [s.done == 1? 0 : 1],
+			whereKey : ["user_id"],
+			whereValue : [s.user_id]
+		}
+	});
+	todos.fetch();
+}
+
+todos && todos.fetch();
+
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,11 +1,11 @@
 var todos = Alloy.Collections.todo;
 todos.fetch();
 
-if(OS_IOS) { 
-   $.navGroupWin.open(); 
-} 
-if (OS_ANDROID) { 
-   $.index.open(); 
+if (OS_IOS) {
+	$.navGroupWin.open();
+}
+if (OS_ANDROID) {
+	$.index.open();
 }
 //$.todoWin.open();
 
@@ -13,28 +13,15 @@ todos.fetch();
 
 // open the "add item" window
 function addToDoItem() {
-	var add=Alloy.createController("add", {}).getView();
-    $.navGroupWin.openWindow(add);
+	var add = Alloy.createController("add", {}).getView();
+	$.navGroupWin.openWindow(add);
 }
 
 // Show task list based on selected status type
-/*
-function showTask(e) {
-	if ( typeof e.index !== 'undefined' && e.index !== null) {
-		whereIndex = e.index;
-		// TabbedBar
-	} else {
-		whereIndex = INDEXES[e.source.title];
-		// Android menu
-	}
-	todos.fetch();
-}
-*/
 // Open bookdetails Window
 function show(event) {// to show bookdetails lists
 	var selectedBook = event.source;
 	var args = {
-
 		user_id : selectedBook.user_id,
 		item : selectedBook.text,
 		date_completed : selectedBook.ph,
@@ -52,8 +39,7 @@ if (OS_ANDROID) {
 	bookview.open();
 }
 
-function tomenu() {
-	var menu=Alloy.createController("menu", {}).getView();
-    $.navGroupWin.openWindow(menu);
+function toFav() {
+	var toFav = Alloy.createController("favourite", {}).getView();
+	$.navGroupWin.openWindow(toFav);
 }
-
