03a336130c6be635e612395b12455d9cecc30ad3
Shwe Sin May Oo <shwesinmayoo@pcp.co.jp>
2015-06-25 16:20:16 +0630

    add progile pic

--- app/controllers/details.js
+++ app/controllers/details.js
@@ -22,3 +22,49 @@ function addItem() {
 function closeWindow() {
 	$.detailWin.close();
 }
+
+$.contactpic.addEventListener("click", function() {
+
+    var opts = {
+        cancel : 2,
+        options : ['Take a Photo', 'Select from Gallery', 'Cancel'],
+        selectedIndex : 2,
+        destructive : 0,
+        title : 'Set a Profile Photo:'
+    };
+
+    var dialog = Ti.UI.createOptionDialog(opts);
+    dialog.show();
+
+    dialog.addEventListener("click", function(e) {
+        switch(e.index) {
+
+            case(0):
+                Titanium.Media.showCamera({
+                    allowEditing : true,
+                    success : function(event) {
+                        $.contactpic.status = "new";
+                        $.contactpic.image = event.media;
+                    },
+
+                    error : function(event) {
+                        console.log(event);
+                    }
+                });
+
+            case(1):
+                Titanium.Media.openPhotoGallery({
+                    allowEditing : true,
+                    success : function(event) {
+                        $.contactpic.status = "new";
+                        $.contactpic.image = event.media;
+                    },
+
+                    error : function(event) {
+                        console.log(event);
+                    }
+                });
+
+            }
+        });
+});
